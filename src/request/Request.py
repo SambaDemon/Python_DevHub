e63e02d5727890a359f88010598d0844f402e761
Aleksander Sukharev <aleksander.sukharev@castingnetworks.com>
2016-05-12 17:42:35 +0300

    Change back to urllib/urllib2

--- src/request/Request.py
+++ src/request/Request.py
@@ -1,23 +1,30 @@
-from __future__ import absolute_import
-
-import jsonpickle
-import requests
 
 from .Config import Config
 from .Utilities import RemoveFromJson
+import jsonpickle
+import ssl
+import sys
+
+if sys.version_info.major < 3:
+    import urllib2 as urllib
+else:
+    import urllib.request, urllib.error, urllib.parse
 
 
 class Request (object):
-
     def __getstate__(self):
         body = RemoveFromJson(self.__dict__.copy())
         return body
 
     def __init__(self, category, proxy, endpoint, method):
         self.queryParams = {}
+
         self.url = ("https://%s/%s/sp2/%s/v%s/%s" %
-                    (Config.baseEndpoint, category,
-                     proxy, Config.version, endpoint))
+                    (Config.baseEndpoint,
+                     category,
+                     proxy,
+                     Config.version,
+                     endpoint))
         self.method = method
 
     def send(self):
@@ -30,25 +37,32 @@ class Request (object):
             queryParamString = ''
             if (self.queryParams):
                 queryParamString = "?"
-                for key, value in self.queryParams.iteritems():
+                for key, value in self.queryParams.items():
                     queryParamString += key + "=" + value + "&"
                 queryParamString = queryParamString[:-1]
             url = self.url + queryParamString
             body = jsonpickle.encode(self, unpicklable=False)
             if (Config.printRequest):
                 print (body)
+            if(Config.proxy):
+                proxy = urllib.request.ProxyHandler(Config.proxy)
+                auth = urllib.request.HTTPBasicAuthHandler()
+                opener = urllib.request.build_opener(proxy,
+                                                     auth,
+                                                     urllib.request.HTTPHandler)
+                urllib.request.install_opener(opener)
+            req = urllib.request.Request(url)
+            req.add_header('Authorization',
+                           'VANTIV license=' + "\"" + Config.license + "\"")
+            req.add_header('Content-Type', 'application/json')
+            context = ssl._create_unverified_context()
             try:
-                headers = {'Authorization':
-                           'VANTIV license='+"\""+Config.license+"\"",
-                           'Content-Type': 'application/json'}
-                response = requests.get(url,
-                                        proxies=Config.proxy,
-                                        data=body,
-                                        headers=headers)
-                code = response.status_code
-                import ipdb; ipdb.set_trace() # DEBUG
-                contents = response.json
-            except Exception as error:
+                resp = urllib.request.urlopen(req,
+                                              body.encode('UTF-8'),
+                                              context=context)
+                code = resp.getcode()
+                contents = resp.read()
+            except urllib.error.HTTPError as error:
                 contents = error.read()
             if (Config.printResponse):
                 print(contents)
