7da6bf634bdd72c986e677f3bd65661fd3d973f6
Aleksander Sukharev <aleksander.sukharev@castingnetworks.com>
2016-06-10 11:32:42 +0300

    Update names

--- vantiv/wrapper.py
+++ vantiv/wrapper.py
@@ -8,127 +8,127 @@ def credit_authorization(**kwargs):
     return authorizationRequest, errors
 
 
-def sampleCreditReversal(**kwargs):
+def credit_reversal(**kwargs):
     reversalRequest, errors = Credit.Reversal.__schema__().load(kwargs)
 
     return reversalRequest, errors
 
 
-def sampleCreditAuthorizationCompletion(**kwargs):
+def credit_authorization_completion(**kwargs):
     authorizationCompletionRequest, errors = Credit.AuthorizationCompletion.\
                                                 __schema__().load(kwargs)
 
     return authorizationCompletionRequest, errors
 
 
-def sampleCreditCaptureGivenAuth(**kwargs):
+def credit_capture_given_auth(**kwargs):
     captureGivenAuthRequest, errors = Credit.CaptureGivenAuth.\
                                      __schema__().load(kwargs)
 
     return captureGivenAuthRequest, errors
 
 
-def sampleCreditCredit(**kwargs):
+def credit_credit(**kwargs):
     creditRequest, errors = Credit.Credit.__schema__().load(kwargs)
 
     return creditRequest, errors
 
 
-def sampleCreditForce(**kwargs):
+def credit_force(**kwargs):
     forceRequest, errors = Credit.Force.__schema__().load(kwargs)
 
     return forceRequest, errors
 
 
-def sampleCreditReturn(**kwargs):
+def credit_return(**kwargs):
     returnRequest, errors = Credit.Return.__schema__().load(kwargs)
 
     return returnRequest, errors
 
 
-def sampleCreditSale(**kwargs):
+def credit_sale(**kwargs):
     saleRequest, errors = Credit.Sale.__schema__().load(kwargs)
 
     return saleRequest, errors
 
 
-def sampleCreditVoid(**kwargs):
+def credit_void(**kwargs):
     voidRequest, errors = Credit.Void.__schema__().load(kwargs)
 
     return voidRequest, errors
 
 
-def sampleCheckCredit(**kwargs):
+def check_credit(**kwargs):
     creditRequest, errors = Check.Credit.__schema__().load(kwargs)
 
     return creditRequest, errors
 
 
-def sampleCheckReturn(**kwargs):
+def check_return(**kwargs):
     returnRequest, errors = Check.Return.__schema__().load(kwargs)
 
     return returnRequest, errors
 
 
-def sampleCheckSale(**kwargs):
+def check_sale(**kwargs):
     saleRequest, errors = Check.Sale.__schema__().load(kwargs)
 
     return saleRequest, errors
 
 
-def sampleCheckVerification(kwargs):
+def check_verification(kwargs):
     verificationRequest, errors = Check.Verification.__schema__().load(kwargs)
 
     return verificationRequest, errors
 
 
-def sampleCheckVoid(**kwargs):
+def check_void(**kwargs):
     voidRequest, errors = Check.Void.__schema__().load(kwargs)
 
     return voidRequest, errors
 
 
-def sampleServicesCreatePlan(**kwargs):
+def services_create_plan(**kwargs):
     createPlanRequest, errors = Services.CreatePlan.__schema__().load(kwargs)
 
     return createPlanRequest, errors
 
 
-def sampleServicesFraudCheck(**kwargs):
+def services_fraud_check(**kwargs):
     fraudCheckRequest, errors = Services.FraudCheck.__schema__().load(kwargs)
 
     return fraudCheckRequest, errors
 
 
-def sampleServicesPaymentAccountCreate(**kwargs):
+def services_payment_account_create(**kwargs):
     paymentAccountCreateRequest, errors = Services.PaymentAccountCreate.\
                                             __schema__().load(kwargs)
 
     return paymentAccountCreateRequest, errors
 
 
-def sampleServicesScheduledTaskDelete(**kwargs):
+def services_scheduled_task_delete(**kwargs):
     scheduledTaskDeleteRequest, errors = Services.ScheduledTaskDelete.\
                                             __schema__().load(kwargs)
 
     return scheduledTaskDeleteRequest, errors
 
 
-def sampleServicesScheduledTaskUpdate(**kwargs):
+def services_scheduled_task_update(**kwargs):
     scheduledTaskUpdateRequest, errors = Services.ScheduledTaskUpdate.\
                                             __schema__().load(kwargs)
 
     return scheduledTaskUpdateRequest, errors
 
 
-def sampleServicesPaymentAccountUpdate(**kwargs):
+def services_payment_account_update(**kwargs):
     paymentAccountUpdateRequest, errors = Services.PaymentAccountUpdate.\
                                             __schema__().load(kwargs)
 
     return paymentAccountUpdateRequest, errors
 
 
-def sampleServicesUpdatePlan(**kwargs):
+def services_update_plan(**kwargs):
         updatePlanRequest, errors = Services.UpdatePlan.\
                                         __schema__().load(kwargs)
 
